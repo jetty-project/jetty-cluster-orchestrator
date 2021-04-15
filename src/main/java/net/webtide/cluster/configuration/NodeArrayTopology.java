b518b5b0baf2c0aea24cbebf7c1fc23d5fd1e487
Ludovic Orban <lorban@bitronix.be>
2021-04-15 20:36:57 +0200

    check for duplicate node ids

--- src/main/java/net/webtide/cluster/configuration/NodeArrayTopology.java
+++ src/main/java/net/webtide/cluster/configuration/NodeArrayTopology.java
@@ -1,21 +1,24 @@
 package net.webtide.cluster.configuration;
 
-import java.util.Arrays;
 import java.util.Collection;
-
-import net.webtide.cluster.configuration.Node;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NodeArrayTopology
 {
-    private final Collection<Node> nodes;
+    private final Map<String, Node> nodes = new HashMap<>();
 
     public NodeArrayTopology(Node... nodes)
     {
-        this.nodes = Arrays.asList(nodes);
+        for (Node node : nodes)
+        {
+            if (this.nodes.put(node.getId(), node) != null)
+                throw new IllegalArgumentException("Duplicate node ID: " + node.getId());
+        }
     }
 
     public Collection<Node> nodes()
     {
-        return nodes;
+        return nodes.values();
     }
 }
