7b6ac96b582c91954351a2f67dbd70fb5f3bad54
Ludovic Orban <lorban@bitronix.be>
2021-04-19 10:22:58 +0200

    improve future array handling

--- src/main/java/org/mortbay/jetty/orchestrator/NodeArrayFuture.java
+++ src/main/java/org/mortbay/jetty/orchestrator/NodeArrayFuture.java
@@ -13,9 +13,13 @@
 
 package org.mortbay.jetty.orchestrator;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class NodeArrayFuture
 {
@@ -26,11 +30,59 @@ public class NodeArrayFuture
         this.futures = futures;
     }
 
-    public void get() throws ExecutionException, InterruptedException
+    public void cancel(boolean mayInterruptIfRunning)
     {
+        futures.forEach(f -> f.cancel(mayInterruptIfRunning));
+    }
+
+    public void get(long timeout, TimeUnit unit) throws ExecutionException
+    {
+        List<Exception> exceptions = new ArrayList<>();
         for (CompletableFuture<Object> future : futures)
         {
-            future.get();
+            try
+            {
+                if (timeout == Long.MIN_VALUE && unit == null)
+                    future.get();
+                else
+                    future.get(timeout, unit);
+            }
+            catch (Exception e)
+            {
+                exceptions.add(e);
+            }
         }
+
+        if (!exceptions.isEmpty())
+        {
+            Exception exception = exceptions.get(0);
+            for (int i = 1; i < exceptions.size(); i++)
+            {
+                Throwable t = exceptions.get(i);
+                exception.addSuppressed(t);
+            }
+            throw new ExecutionException(exception);
+        }
+    }
+
+    public void get() throws CancellationException, ExecutionException
+    {
+        get(Long.MIN_VALUE, null);
+    }
+
+    public boolean isAllDone()
+    {
+        return futures.stream()
+            .map(Future::isDone)
+            .reduce((b1, b2) -> b1 && b2)
+            .orElse(true);
+    }
+
+    public boolean isAnyDone()
+    {
+        return futures.stream()
+            .map(Future::isDone)
+            .reduce((b1, b2) -> b1 || b2)
+            .orElse(true);
     }
 }
